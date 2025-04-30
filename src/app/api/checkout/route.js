cfb7b01f47786115b75fcafcfc2e67147546d59e
ihrat pramudya <ihratpramudya>
2025-04-30 14:34:11 +0900

    add checkout trasaction api

---
+++ src/app/api/checkout/route.js
@@ -0,0 +1,82 @@
+import { verifyJWT } from "@/lib/utils";
+import { NextResponse } from "next/server";
+import { db } from "@/lib/db";
+
+export async function POST(request) {
+    try {
+        const data = await request.json();
+        const token = request?.cookies?.get("customer_jwt_token")?.value;
+        const decodedToken = await verifyJWT(token);
+
+        if (!decodedToken) {
+            return NextResponse.json(
+                {
+                    message: "Unauthorized!",
+                },
+                { status: 401 }
+            )
+        }
+
+        await db.buyerMaster.update({
+            where: {
+                email: data.customerEmail,
+            },
+            data: {
+                address: data.address,
+                city: data.city 
+            }
+        });
+
+        for(const product of data.products) {
+            const currentproduct = await db.product.findUnique({
+                where: {
+                    id: parseInt(product.id)
+                }
+            });
+
+            const newSizeQuantity = currentproduct?.[product.sizes] - product.quantity;
+            const newCurrentStock = currentproduct?.currentStock - product.quantity;
+        
+            await db.product.update({
+                where: {
+                    id: parseInt(product.id)
+                },
+                data: {
+                    [product.sizes] : newSizeQuantity,
+                    currentStock: newCurrentStock
+                }
+            })
+        }
+
+        const SODateTime = new Date(data.SODateTime * 1000);
+        const salesData = await db.salesMaster.create({
+            data: {
+                bId: parseInt(data.customerId),
+                SODateTime: SODateTime,
+                grandTotalPrice: data.grandTotalPrice,
+                paymentMode: data.paymentMode
+            }
+        });
+
+        for (const product of data.products) {
+            await db.salesTransaction.create({
+                data: {
+                    SMOId: salesData.id,
+                    productId: parseInt(product.id),
+                    productName: product.name,
+                    unitPrice: product.sellPrice,
+                    qtyPurchased:  product.quantity,
+                    total: product.quantity * product.sellPrice,
+                }
+            })
+        }
+    } catch(error) {
+        return NextResponse.json(
+            {
+                message: "Something Went Wrong",
+                error: error.message
+            },
+            { status: 500 }
+        )
+    }
+}
